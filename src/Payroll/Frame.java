be28f973b94905f3705de9a2f19a33390ea0eeb7
ahmedbassemaly <76752860+ahmedbassemaly@users.noreply.github.com>
2021-01-12 00:04:42 +0200

    backButton

--- src/Payroll/Frame.java
+++ src/Payroll/Frame.java
@@ -23,7 +23,7 @@ public class Frame extends JFrame implements Files {
     JButton teamleader = new JButton("Team Leader & Team Member");
     JButton manager = new JButton("Manager");
     JButton submit = new JButton("Submit");
-    Icon backIcon = new ImageIcon("C:\\Users\\Mohammed Yasser\\Desktop\\MIU\\ProjectFrames\\BackButton.png");
+    Icon backIcon = new ImageIcon("pic1.JPEG");
     JButton back = new JButton(backIcon);
     //Manager
     JButton Trainee = new JButton("Trainee");
